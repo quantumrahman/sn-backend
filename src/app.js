ab4d7ab7992814940b1ac4cc1d086a8edfc5c206
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-25 21:54:48 +0600

    feat: implement facility and booking routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 
+import facilityRoute from './routers/facilityRoutes.js';
+import bookingRoute from './routers/bookingRoutes.js';
+
 const app = express();
 
 app.use(express.json());
@@ -8,5 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.status(200).json({ success: true, message: 'server running properly' });
 });
+
+app.use('/api/v1', facilityRoute);
+app.use('/api/v1', bookingRoute);
 
 export default app;

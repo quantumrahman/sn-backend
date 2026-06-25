c83bca2ae6c81fe4726cbb2bc83367eaef9c5de0
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-25 21:36:10 +0600

    feat: implement express server instance

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.status(200).json({ success: true, message: 'server running properly' });
+});
+
+export default app;

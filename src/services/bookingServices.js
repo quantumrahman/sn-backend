890561d7869c5fd893cbeefdd53c4eabdd247e3f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-25 22:29:42 +0600

    feat: implement booking services function

---
+++ src/services/bookingServices.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+export const createService = async (payload) => {
+    console.log('create service');
+};
+
+export const readsService = async () => {
+    console.log('reads service');
+};
+
+export const readService = async (id) => {
+    console.log('read service');
+};
+
+export const updateService = async (id, payload) => {
+    console.log('update service');
+};

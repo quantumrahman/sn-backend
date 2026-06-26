e99ac46bff32b6112e1a2d1c9920fe574dfbb459
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-26 10:46:12 +0600

    feat: implement create facility service business logic

--- src/services/facilityServices.js
+++ src/services/facilityServices.js
@@ -1,7 +1,26 @@
 /* eslint-disable no-unused-vars */
 
+import Facility from '../models/facilityModel.js';
+import AppError from '../utils/appError.js';
+
 export const createService = async (payload) => {
-    console.log('create service');
+    const facilityExists = await Facility.findOne({
+        name: payload.name,
+    }).lean();
+
+    if (facilityExists) {
+        throw new AppError(400, 'Facility already exists');
+    }
+
+    const available_slots = payload.available_slots
+        .split(',')
+        .map((slot) => slot.trim())
+        .filter(Boolean);
+
+    return await Facility.create({
+        ...payload,
+        available_slots: available_slots,
+    });
 };
 
 export const readsService = async () => {
